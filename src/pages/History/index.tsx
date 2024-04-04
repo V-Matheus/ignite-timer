0ce7bf2a4dde0adf1e21848774fff6de26394329
Victor Matheus <victormatheus507@gmail.com>
2024-04-04 14:48:12 -0300

    component: status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
   return (
@@ -19,35 +19,36 @@ export function History() {
                 <td>Tarefa</td>
                 <td>Duração</td>
                 <td>Há cerca de 2 meses</td>
-                <td>Concluido</td>
+                <td><Status statusColor="green">Concluido</Status></td>
               </tr>
 
               <tr>
                 <td>Tarefa</td>
                 <td>Duração</td>
                 <td>Há cerca de 2 meses</td>
-                <td>Concluido</td>
+                <td><Status statusColor="green">Concluido</Status></td>
               </tr>
 
               <tr>
                 <td>Tarefa</td>
                 <td>Duração</td>
                 <td>Há cerca de 2 meses</td>
-                <td>Concluido</td>
+                <td><Status statusColor="green">Concluido</Status></td>
               </tr>
 
               <tr>
                 <td>Tarefa</td>
                 <td>Duração</td>
                 <td>Há cerca de 2 meses</td>
-                <td>Concluido</td>
+                <td><Status statusColor="yellow">Em andamento</Status></td>
+                
               </tr>
 
               <tr>
                 <td>Tarefa</td>
                 <td>Duração</td>
                 <td>Há cerca de 2 meses</td>
-                <td>Concluido</td>
+                <td><Status statusColor="red">Interrompido</Status></td>
               </tr>
             </tbody>
           </table>
