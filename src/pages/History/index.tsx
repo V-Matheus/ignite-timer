b0a7792f24b8e540ecd97b8e52940d43500f2f89
Victor Matheus <victormatheus507@gmail.com>
2024-04-04 21:49:23 -0300

    formatacao de data

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,6 @@
 import { useContext } from 'react';
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale/pt-BR';
 import { HistoryContainer, HistoryList, Status } from './styles';
 import { CycleContext } from '../../contexts/CyclesContext';
 
@@ -24,7 +26,12 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
                   <td>
                     {cycle.fineshedDate && (
                       <Status statusColor="green">Concluido</Status>
